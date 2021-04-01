2e46146c0dfb4d82bb8229e9ae87a2faf2692f00
souljungkim <souljungkim@gmail.com>
2021-04-01 22:23:32 +0900

    update test that SimpleDateFormat is not thread safe

--- src/test/java/com/jaemisseo/proving/SimpleDateFormatIsNotThreadSafeTest.java
+++ src/test/java/com/jaemisseo/proving/SimpleDateFormatIsNotThreadSafeTest.java
@@ -1,51 +1,79 @@
 package com.jaemisseo.proving;
 
+import com.jaemisseo.test.util.JaemisseoTestUtil;
 import org.junit.jupiter.api.Test;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
-import java.util.concurrent.*;
+import java.util.concurrent.ExecutionException;
 
 /**************************************************
  *
- * SimpleDateFormat never
- *	- It is not thread safe.
- *  - Never use DateFormat, SimpleDateFormat, Date, and Calendar classes. These terrible classes are all legacy now. They were supplanted years ago by the modern java.time classes defined in JSR 310. The java.time classes are thread-safe by design, using immutable objects
- *  - this hasn't been fixed in JDK8 per se. but JDK8 introduces the new java.time package, including DateTimeFormatter which is threadsafe.
+ * SimpleDateFormat is not thread safe.
+ *
+ *	- Someone says: "It is not thread safe."
+ *  - Someone says: "Never use DateFormat, SimpleDateFormat, Date, and Calendar classes. These terrible classes are all legacy now. They were supplanted years ago by the modern java.time classes defined in JSR 310. The java.time classes are thread-safe by design, using immutable objects"
+ *  - Someone says: "this hasn't been fixed in JDK8 per se. but JDK8 introduces the new java.time package, including DateTimeFormatter which is threadsafe."
  *
  **************************************************/
-public class SimpleDateFormatIsNotThreadSafeTest {
+public class SimpleDateFormatIsNotThreadSafeTest extends JaemisseoTestUtil {
 
 	final static int threadPoolSize = 50;
 	final static int threadCount = 10000;
 
+
+	/**************************************************
+	 *
+	 * [Not Thread Safe] case
+	 * 		- All of threads use only one instance
+	 *
+	 **************************************************/
 	@Test
-	public void SimpleDateFormatIsNotSafeTest() {
+	public void not_threadSafe() {
+		//Define - only one instance
 		final SimpleDateFormat format = new SimpleDateFormat("yyyyMMdd");
 
-		final List<Class<? extends Exception>> occurableExceptions = Arrays.asList(NumberFormatException.class, ArrayIndexOutOfBoundsException.class);
+		//Define - occurable exceptions
+		final List<Class<? extends Exception>> occurableExceptions = Arrays.asList(
+				NumberFormatException.class,
+				ArrayIndexOutOfBoundsException.class
+		);
 
-		//Run Threads
+		//Assert - Throw during running threads
 		assertThrowWithThreads(occurableExceptions, threadPoolSize, threadCount, (count) -> {
 			return format.parse("20150630");
 		});
 	}
 
+
+
+	/**************************************************
+	 *
+	 * [Thread Safe] case
+	 *		- use new everytime
+	 *		- use synchronized
+	 *		- use method - SimpleDateFormat.getDateInstance(..)
+	 *		- use threadLocal
+	 *
+	 **************************************************/
 	@Test
-	public void SimpleDateFormatIsSafeTest() throws InterruptedException, ExecutionException {
+	public void threadSafe_every_new_instance() throws InterruptedException, ExecutionException {
 		//Run Threads
 		List<Date> results = runThreads(threadPoolSize, threadCount, (count) -> {
 			return new SimpleDateFormat("yyyyMMdd").parse("20150630"); // 정상 동작시키기 위해서는 이와 같이 사용해야 함
 		});
+
+		//Assert
 		assert results.size() == threadCount;
+		assert results.stream().allMatch( (it) -> it instanceof Date );
 	}
 
 	@Test
-	public void synchronizedSimpleDateFormat() throws InterruptedException, ExecutionException {
+	public void threadSafe_synchronized() throws InterruptedException, ExecutionException {
+		//Define - only one instance
 		final SimpleDateFormat format = new SimpleDateFormat("yyyyMMdd");
 
 		//Run Threads
@@ -57,28 +85,29 @@ public class SimpleDateFormatIsNotThreadSafeTest {
 			return result;
 		});
 
+		//Assert
 		assert results.size() == threadCount;
 		assert results.stream().allMatch( (it) -> it instanceof Date );
 	}
 
 	@Test
-	public void getDataInstance() throws InterruptedException, ExecutionException {
+	public void threadSafe_getDateInstance() throws InterruptedException, ExecutionException {
 		//Run Threads
 		List<Date> results = runThreads(threadPoolSize, threadCount, (count) -> {
 			return SimpleDateFormat.getDateInstance(DateFormat.MEDIUM).parse("2015. 06. 30");
 		});
 
+		//Assert
 		assert results.size() == threadCount;
 		assert results.stream().allMatch( (it) -> it instanceof Date );
 	}
 
 
 	@Test
-	public void makeThreadSafeTaskAsThreadLocal() throws InterruptedException, ExecutionException {
+	public void threadSafe_threadLocal() throws InterruptedException, ExecutionException {
 		//Setup threadLocal
 		ThreadLocal<DateFormat> format = new ThreadLocal<DateFormat> () {
-			@Override
-			protected DateFormat initialValue() {
+			@Override protected DateFormat initialValue() {
 				return new SimpleDateFormat("yyyyMMdd");
 			}
 		};
@@ -90,66 +119,6 @@ public class SimpleDateFormatIsNotThreadSafeTest {
 
 		assert results.size() == threadCount;
 		assert results.stream().allMatch( (it) -> it instanceof Date );
-	}
-
-
-
-	@FunctionalInterface
-	public interface FunctionWithException<T, R> {
-		R apply(T t) throws Exception;
-	}
-
-	private <T> List<T> runThreads(int threadPoolSize, int threadCount, FunctionWithException<Integer, T> fwe) throws InterruptedException, ExecutionException {
-		ExecutorService exec = Executors.newFixedThreadPool(threadPoolSize);
-		List<Future<T>> futures = new ArrayList<Future<T>>();
-		List<T> results = new ArrayList<T>();
-		try {
-			// Count만큼 수행
-			for (int i = 0; i< threadCount; i++) {
-				final int count = i + 1;
-				Callable<T> newCallable = new Callable<T>(){
-					@Override
-					public T call() throws Exception {
-						System.out.println("[" +count+ "] " + Thread.currentThread().getName());
-						return fwe.apply(count);
-					}
-				};
-				futures.add( exec.submit(newCallable) );
-			}
-			// 결과 출력
-			for (Future<T> future : futures) {
-				results.add( future.get() );
-			}
-
-		} catch (InterruptedException ie) {
-			throw ie;
-		} catch (ExecutionException ee) {
-			throw ee;
-		} finally {
-			exec.shutdown();
-		}
-		return results;
-	}
-
-	private void assertThrowWithThreads(Class<? extends Exception> ocurrableExceptions, int threadPoolSize, int threadCount, FunctionWithException<Integer, ?> fwe) throws AssertionError {
-		assertThrowWithThreads(Arrays.asList(ocurrableExceptions), threadPoolSize, threadCount, fwe);
-	}
-	private void assertThrowWithThreads(List<Class<? extends Exception>> ocurrableExceptions, int threadPoolSize, int threadCount, FunctionWithException<Integer, ?> fwe) throws AssertionError {
-		try {
-			runThreads(threadPoolSize, threadCount, fwe);
-
-		}catch (ExecutionException ee) {
-			ee.printStackTrace();
-			boolean anyMatched = ocurrableExceptions.stream().anyMatch( (it) -> ee.getCause().getClass().isAssignableFrom(it) );
-			if (!anyMatched)
-				throw new AssertionError();
-
-		}catch(Exception e){
-			e.printStackTrace();
-			boolean anyMatched = ocurrableExceptions.stream().anyMatch( (it) -> e.getCause().getClass().isAssignableFrom(it) );
-			if (!anyMatched)
-				throw new AssertionError();
-		}
 	}
 
 }
